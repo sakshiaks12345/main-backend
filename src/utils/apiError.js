c2234c9683c7cdda532747997e5936279e6ef5b3
sakshiaks12345 <sakshiaks12345@gmail.com>
2026-01-20 01:44:07 +0530

    all setup configured

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -2,8 +2,8 @@ class ApiError extends Error {
     constructor(
         statusCode,
         message= "Something went wrong",
-        error = [],
-        statck = ""
+        errors = [],
+        stack = ""
     ){
         super(message)
         this.statusCode = statusCode
@@ -12,8 +12,8 @@ class ApiError extends Error {
         this.success = false;
         this.error = errors;
 
-        if (Statck) {
-            this.stack = statck
+        if (stack) {
+            this.stack = stack
         } else {
             Error.captureStackTrace(this, this.constructor)
         }
