1d5764be6db06fc5c35582359133d49ac8a723db
sakshiaks12345 <sakshiaks12345@gmail.com>
2026-01-21 02:31:27 +0530

    add register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,7 +52,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next()
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
@@ -86,4 +86,4 @@ userSchema.methods.generateRefreshToken = async function () {
     )
 }
 
-export const User = mongoose.models('User', userSchema);
+export const User = mongoose.model('User', userSchema);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -40,4 +40,4 @@ const videoSchema = new Schema(
 
 videoSchema.plugin(mongooseAggregatePaginate)
 
-export const Video = mongoose.models('Video', videoSchema);
+export const Video = mongoose.model('Video', videoSchema);
