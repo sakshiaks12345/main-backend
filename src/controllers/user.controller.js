070a4c78bd49f6e10d8f4dfeb818ce061ab7bc32
sakshiaks12345 <sakshiaks12345@gmail.com>
2026-01-21 13:41:11 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -33,8 +33,14 @@ const registerUser = asyncHandler(async(req, res)=>{
         throw new ApiError(409, "User with email or username already exists")
     }
     
-    const avatarLocalPath = req.file?.avatar[0]?.path;
-    const coverImageLocalPath = req.file?.coverImage[0]?.path;
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // console.log(req.files);
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0 ) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+    
     
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
@@ -53,7 +59,7 @@ const registerUser = asyncHandler(async(req, res)=>{
         coverImage : coverImage?.url || "",
         email,
         password,
-        username: username.toLowercase()
+        username: username.toLowerCase()
     })
 
     const createdUser = await User.findById(user._id).select( "-password -refreshToken")
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,8 @@ const uploadOnCloudinary = async (localFilePath) =>{
             resource_type: "auto"
         })
         //file has been uploaded successfull
-        console.log("file is uploaded on cloudnary", response.url);
+        // console.log("file is uploaded on cloudnary", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
         
     } catch (error) {
